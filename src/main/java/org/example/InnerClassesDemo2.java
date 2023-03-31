d8e9cf61cc7087ebef32e38daa2d9034d49f6fba
USER <rajputshubham615@gmail.com>
2023-03-31 17:44:51 +0530

    Initial commit

---
+++ src/main/java/org/example/InnerClassesDemo2.java
@@ -0,0 +1,23 @@
+package org.example;
+
+public class InnerClassesDemo {
+    public static void main(String[] args) {
+       Outer outer=new Outer();
+       outer.test();
+    }
+
+}
+class Outer{
+    int outer_x=100;
+    void test(){
+        Inner inner=new Inner();
+        inner.display();
+
+    }
+    class Inner{
+        void display(){
+            System.out.println("display: outer_x = " + outer_x);
+        }
+    }
+}
+
